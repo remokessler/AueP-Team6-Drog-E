767b254c56c6d572ee66a98745b445b0c3459eda
Remo Kessler <remo.kessler@gmx.ch>
2022-01-17 16:24:25 +0100

    login

--- it/src/dark-web-bnl/src/api-authorization/api-authorization.module.ts
+++ it/src/dark-web-bnl/src/api-authorization/api-authorization.module.ts
@@ -6,24 +6,26 @@ import { LogoutComponent } from './logout/logout.component';
 import { RouterModule } from '@angular/router';
 import { ApplicationPaths } from './api-authorization.constants';
 import { HttpClientModule } from '@angular/common/http';
+import {BsDropdownModule} from "ngx-bootstrap/dropdown";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    HttpClientModule,
-    RouterModule.forChild(
-      [
-        { path: ApplicationPaths.Register, component: LoginComponent },
-        { path: ApplicationPaths.Profile, component: LoginComponent },
-        { path: ApplicationPaths.Login, component: LoginComponent },
-        { path: ApplicationPaths.LoginFailed, component: LoginComponent },
-        { path: ApplicationPaths.LoginCallback, component: LoginComponent },
-        { path: ApplicationPaths.LogOut, component: LogoutComponent },
-        { path: ApplicationPaths.LoggedOut, component: LogoutComponent },
-        { path: ApplicationPaths.LogOutCallback, component: LogoutComponent }
-      ]
-    )
-  ],
+    imports: [
+        CommonModule,
+        HttpClientModule,
+        RouterModule.forChild(
+            [
+                {path: ApplicationPaths.Register, component: LoginComponent},
+                {path: ApplicationPaths.Profile, component: LoginComponent},
+                {path: ApplicationPaths.Login, component: LoginComponent},
+                {path: ApplicationPaths.LoginFailed, component: LoginComponent},
+                {path: ApplicationPaths.LoginCallback, component: LoginComponent},
+                {path: ApplicationPaths.LogOut, component: LogoutComponent},
+                {path: ApplicationPaths.LoggedOut, component: LogoutComponent},
+                {path: ApplicationPaths.LogOutCallback, component: LogoutComponent}
+            ]
+        ),
+        BsDropdownModule
+    ],
   declarations: [LoginMenuComponent, LoginComponent, LogoutComponent],
   exports: [LoginMenuComponent, LoginComponent, LogoutComponent]
 })
