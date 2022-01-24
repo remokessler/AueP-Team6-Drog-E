1ccf978f7c2b27961cce9d80c3950c42987b153b
Remo Kessler <remo.kessler@gmx.ch>
2022-01-24 14:53:11 +0100

    razer RGB support

--- src/dark-web-bnl/src/app/app.module.ts
+++ src/dark-web-bnl/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
@@ -12,9 +11,19 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.module';
 import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
-import {AppAsideModule, AppBreadcrumbModule, AppFooterModule, AppHeaderModule, AppSidebarModule} from "@coreui/angular";
-import {PerfectScrollbarModule} from "ngx-perfect-scrollbar";
-import {TabsModule} from "ngx-bootstrap/tabs";
+import {
+  AppAsideModule,
+  AppBreadcrumbModule,
+  AppFooterModule,
+  AppHeaderModule,
+  AppSidebarModule
+} from '@coreui/angular';
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { AnimationBuilder } from '@angular/animations';
 
 @NgModule({
   declarations: [
@@ -25,14 +34,14 @@ import {TabsModule} from "ngx-bootstrap/tabs";
     FetchDataComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({appId: 'ng-cli-universal'}),
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     ApiAuthorizationModule,
     RouterModule.forRoot([
-      {path: '', component: HomeComponent, pathMatch: 'full'},
-      {path: 'counter', component: CounterComponent},
-      {path: 'fetch-data', component: FetchDataComponent, canActivate: [AuthorizeGuard]},
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'counter', component: CounterComponent },
+      { path: 'fetch-data', component: FetchDataComponent, canActivate: [AuthorizeGuard] }
     ]),
     AppHeaderModule,
     AppSidebarModule,
@@ -40,11 +49,15 @@ import {TabsModule} from "ngx-bootstrap/tabs";
     AppBreadcrumbModule,
     AppAsideModule,
     TabsModule,
-    AppFooterModule
+    AppFooterModule,
+    BsDropdownModule,
+    ModalModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthorizeInterceptor, multi: true }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
