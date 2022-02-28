0f7fc575e971188a6047d6934b42964d8991bf2b
Remo Kessler <remo.kessler@gmx.ch>
2022-02-28 14:57:32 +0100

    Added bitcoin miner

--- src/bnl-dark-api/bnl-dark-api/Models/ApplicationUser.cs
+++ src/bnl-dark-api/bnl-dark-api/Models/ApplicationUser.cs
@@ -1,7 +1,13 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
 
 namespace bnl_dark_api.Models;
 
 public class ApplicationUser : IdentityUser
 {
+    
+    [DatabaseGenerated(DatabaseGeneratedOption.None)]
+    [StringLength(50)]
+    public override string Id { get; set; }
 }
--- src/bnl-dark-api/bnl-dark-api/Program.cs
+++ src/bnl-dark-api/bnl-dark-api/Program.cs
@@ -10,8 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
+    options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
